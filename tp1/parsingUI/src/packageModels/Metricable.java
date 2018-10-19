f5f4eca67b2813ebc96c63a0f93a3ded54bb39a5
Marc Laliberté <mlaliberte@simaudio.com>
2018-10-18 21:43:11 -0400

    added Metricable and ErrorModelable

---
+++ tp1/parsingUI/src/packageModels/Metricable.java
@@ -0,0 +1,47 @@
+package packageModels;
+
+import java.util.*;
+
+public interface Metricable {
+	
+	//ANA
+	int getAverageMethodArgumentCount();
+		
+	//NOM
+	int getMethodCount();
+	
+	//NOA
+	int getAttributeCount();
+	
+	//ITC
+	int getModelableArgumentCount();
+	
+	//ETC
+	int getTimesUsedAsArgument();
+	
+	//CAC
+	int getAssociationCount();
+	
+	//DIT
+	int getLongestPathLengthToRoot();
+	
+	//DLC
+	int getLongestPathLengthtoLeaf();
+	
+	//NOC
+	int getDirectSubClassCount();
+	
+	//NOD
+	int getSubClassCount();
+	
+	List<String> getAllMetrics();
+	
+	//helpers
+	int getMethodArgumentCount();
+	
+	
+	
+	
+	
+
+}
